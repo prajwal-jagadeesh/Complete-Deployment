02f94497077add41abec71688b913854fb8ee502
prajwal <prajwal.j91@gmail.com>
2024-03-08 18:43:40 +0530

    New Changes

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 module "ec2" {
-  source            = "./modules/ec2"
-  ami               = var.ami
-  instance_type     = var.instance_type
-  security_group_id = module.sg.security_group_id
-  subnet_id         = module.EKS-VPC.public-subnet1
+  source        = "./modules/ec2"
+  ami           = var.ami
+  instance_type = var.instance_type
+  ssh_port      = var.ssh_port
+  http_port     = var.http_port
+  https_port    = var.https_port
+  protocol      = var.protocol
 }
 
 module "sg" {
