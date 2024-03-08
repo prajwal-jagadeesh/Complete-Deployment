8d730a4dbb9f93d47d7ead0d498a0f6a6ec4305b
prajwal <prajwal.j91@gmail.com>
2024-03-08 17:15:03 +0530

    New Changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   count           = 4
   ami             = var.ami
   instance_type   = var.instance_type
-  security_groups = var.security_group_id
+  security_groups = [ var.security_group_id ]
   key_name        = "webapp"
 
   tags = {
