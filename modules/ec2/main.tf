ce32aa1cc20cad85810bad00433856d7ce69dd48
prajwal <prajwal.j91@gmail.com>
2024-03-08 16:39:10 +0530

    New changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,18 +2,7 @@ resource "aws_instance" "ec2" {
   count         = 4
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = "webapp"
-
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo apt-get update -y
-                sudo apt-get install unzip -y
-                curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                unzip awscliv2.zip
-                chmod 700 ~/.ssh
-                chmod 600 ~/.ssh/authorized_keys
-              EOF
-
+  
   tags = {
     Name = "${var.instance_names[count.index]}-${terraform.workspace}"
   }
@@ -30,8 +19,8 @@ resource "null_resource" "copy_ssh_key" {
 
     connection {
       type        = "ssh"
-      user        = "ubuntu"                # Change if your instance uses a different username
-      private_key = file("~/.ssh/id_rsa")     # Path to your private key
+      user        = "ubuntu"              # Change if your instance uses a different username
+      private_key = file("~/.ssh/id_rsa") # Path to your private key
       host        = aws_instance.ec2[count.index].public_ip
     }
   }
