79c314f6a91282a51b5c739adeed6939a9d5585d
prajwal <prajwal.j91@gmail.com>
2024-03-08 17:59:22 +0530

    New Changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "ec2" {
   ami                         = var.ami
   instance_type               = var.instance_type
   vpc_security_group_ids      = [var.security_group_id]
-  key_name                    = "webapp"
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
 
