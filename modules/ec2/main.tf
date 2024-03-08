976a39d1ff28db59cde27b3ea7475374ff468746
prajwal <prajwal.j91@gmail.com>
2024-03-08 16:37:38 +0530

    New changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,17 +2,20 @@ resource "aws_instance" "ec2" {
   count         = 4
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = "webapp"
 
   user_data = <<-EOF
                 #!/bin/bash
                 sudo apt-get update -y
-                sudo apt-get install -y awscli
+                sudo apt-get install unzip -y
+                curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+                unzip awscliv2.zip
                 chmod 700 ~/.ssh
                 chmod 600 ~/.ssh/authorized_keys
               EOF
 
   tags = {
-    Name = "${var.instance_names[count.index]}-${terraform.workspace}-${count.index}"
+    Name = "${var.instance_names[count.index]}-${terraform.workspace}"
   }
 }
 
@@ -20,16 +23,16 @@ resource "aws_instance" "ec2" {
 resource "null_resource" "copy_ssh_key" {
   count = length(aws_instance.ec2)
 
-  connection {
-    type        = "ssh"
-    host        = aws_instance.ec2[count.index].public_ip
-    user        = "ubuntu"                            # Change to your desired username
-    private_key = file("~/.ssh/id_rsa")                # Change to your private key path
-  }
-    
   provisioner "remote-exec" {
     inline = [
-      "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDkndMMNnne0tjbizpvk3ybU9pVXUdCIOlp8TNJq5f1cbZm9HUs73OdPfjNauiPZWSYuZzOEYPSVq0Dg1NAvTb6xCrR/ML1FXFqaSaLgDrggayoy326rEYuwysFbPPcuda5GYTGmbH8txn+VX+Bh2ct3rJbBAK8G/9L+oZxkMmT3wqkW9uDyFbtFaNaYE31xvw3bIrMM6K7twuM++rmD2/nUF8sSX7TtDIAFQXOYxNB7Ie8tNAOGgtlitfgeTfADRhWEBv5nhjehqBg3bnF4SzcFbQQAOuYjXyIcAKceKHysmTs1dCy8R3obOUinlrAUl+i1pKXWz9H9KT4t0YJO1oR8A7DGKR8vtFUAJrrzRj06beQp3xJG0Hx9nJTNXvuaKV8ENl6Te9hLt/wn3+VKs/Tr/HudHhenHwSTqHmTZ5kGrV4ot4+PeoyscFdJtTPZIoz/tCGlEQgpiwADaAKG+IbCt4/5/4bqb5c3VwxQKtAazXFyvyFo5CcOA0iAPJq05eXq7Y6JjJciXojrUD4MSPPEREGEKA+fHPhUhg+rNOL2T96dKi//aPiyUHID6cHjVIeLHba+iYgpF+f/IXrgSWJqXm9JBnEmotwSrbKrys3RSR9lOc4acGUUeRfGxEsCv845sAGvVFHz4UDHh57tn8Z6pTkM+UXx17SeQVN1qJ69Q== ubuntu@ip-172-31-14-242' >> ~/.ssh/authorized_keys" # Change to your public key
+      "echo '${file("~/.ssh/id_rsa.pub")}' >> ~/.ssh/authorized_keys",
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"                # Change if your instance uses a different username
+      private_key = file("~/.ssh/id_rsa")     # Path to your private key
+      host        = aws_instance.ec2[count.index].public_ip
+    }
   }
 }
