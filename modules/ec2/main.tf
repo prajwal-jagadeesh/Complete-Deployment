39949dc46e7b4442f25c54835199181a5542eede
prajwal <prajwal.j91@gmail.com>
2024-03-08 16:51:19 +0530

    New changes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "ec2" {
   count         = 4
   ami           = var.ami
   instance_type = var.instance_type
-  
+  key_name      = "webapp"
+
   tags = {
     Name = "${var.instance_names[count.index]}-${terraform.workspace}"
   }
@@ -15,13 +16,15 @@ resource "null_resource" "copy_ssh_key" {
   provisioner "remote-exec" {
     inline = [
       "echo '${file("~/.ssh/id_rsa.pub")}' >> ~/.ssh/authorized_keys",
+      "chmod 700 ~/.ssh",
+      "chmod 600 ~/.ssh/authorized_keys"
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"              # Change if your instance uses a different username
       private_key = file("~/.ssh/id_rsa") # Path to your private key
-      host        = aws_instance.ec2[count.index].public_ip
+      host        = self.public_ip
     }
   }
 }
