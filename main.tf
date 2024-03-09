584dc403678113128949c58337c6edf3041aa078
prajwal <prajwal.j91@gmail.com>
2024-03-09 13:59:06 +0530

    New changes

--- main.tf
+++ main.tf
@@ -8,28 +8,28 @@ module "ec2" {
   protocol      = var.protocol
 }
 
-module "sg" {
-  source     = "./modules/sg"
-  ssh_port   = var.ssh_port
-  http_port  = var.http_port
-  https_port = var.https_port
-  protocol   = var.protocol
-  vpc_id     = module.EKS-VPC.VPC_id
-}
+# module "sg" {
+#   source     = "./modules/sg"
+#   ssh_port   = var.ssh_port
+#   http_port  = var.http_port
+#   https_port = var.https_port
+#   protocol   = var.protocol
+#   vpc_id     = module.EKS-VPC.VPC_id
+# }
 
-module "EKS-VPC" {
-  source               = "./modules/VPC"
-  vpc_cidr             = var.vpc_cidr
-  instance_tenancy     = var.instance_tenancy
-  dns_support          = var.dns_support
-  dns_hostnames        = var.dns_hostnames
-  public-subnet1-cidr  = var.public-subnet1-cidr
-  public-subnet2-cidr  = var.public-subnet2-cidr
-  private-subnet1-cidr = var.private-subnet1-cidr
-  private-subnet2-cidr = var.private-subnet2-cidr
-  subnet-az1           = var.subnet-az1
-  subnet-az2           = var.subnet-az2
-}
+# module "EKS-VPC" {
+#   source               = "./modules/VPC"
+#   vpc_cidr             = var.vpc_cidr
+#   instance_tenancy     = var.instance_tenancy
+#   dns_support          = var.dns_support
+#   dns_hostnames        = var.dns_hostnames
+#   public-subnet1-cidr  = var.public-subnet1-cidr
+#   public-subnet2-cidr  = var.public-subnet2-cidr
+#   private-subnet1-cidr = var.private-subnet1-cidr
+#   private-subnet2-cidr = var.private-subnet2-cidr
+#   subnet-az1           = var.subnet-az1
+#   subnet-az2           = var.subnet-az2
+# }
 
 # module "EKS" {
 #   source            = "./modules/EKS"
