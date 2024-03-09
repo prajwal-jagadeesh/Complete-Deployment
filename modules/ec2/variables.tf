7f3816d3084f7b0a40dbd73039684733ef4e6d7c
prajwal <prajwal.j91@gmail.com>
2024-03-09 14:05:55 +0530

    New changes

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,12 +2,6 @@ variable "ami" {
   description = "AMI"
 }
 
-variable "ssh_port" {
-  description = "The port the EC2 Instance should listen on for SSH requests."
-  type        = number
-  default     = 22
-}
-
 variable "ssh_user" {
   description = "SSH user name to use for remote exec connections,"
   type        = string
