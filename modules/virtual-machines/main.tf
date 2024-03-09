4451d3317bcafb9429f0ca97fa72fd062f236c4c
prajwal <prajwal.j91@gmail.com>
2024-03-09 15:47:40 +0530

    New changes

--- modules/virtual-machines/main.tf
+++ modules/virtual-machines/main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "web" {
     Environment = "dev"
   }
 
-  key_name        = aws_key_pair.terraform-demo.key_name
-  vpc_security_group_ids = [ aws_security_group.allow_ssh_http_sg.id ]
+  key_name               = aws_key_pair.terraform-demo.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http_sg.id]
 }
 
 resource "null_resource" "hosts" {
@@ -20,19 +20,19 @@ resource "null_resource" "hosts" {
   count = length(aws_instance.web)
 
   connection {
-    type = "ssh"
-    host = aws_instance.web[count.index].public_ip
-    user = "ubuntu"
-    private_key = "${file("~/.ssh/id_rsa")}"
-    agent = false
+    type        = "ssh"
+    host        = aws_instance.web[count.index].public_ip
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    agent       = false
   }
 
   provisioner "remote-exec" {
-    inline = [ 
+    inline = [
       "echo ${file("~/.ssh/id_rsa.pub")}' >> ~/.ssh/authorized_keys",
       "chmod 700 ~/.ssh",
       "chmod 600 ~/.ssh/authorized_keys"
-     ]
+    ]
   }
 
   provisioner "local-exec" {
