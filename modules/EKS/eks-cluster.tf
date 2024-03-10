35c27b2b3115c1010b9dc0a6245816039ee83237
prajwal <prajwal.j91@gmail.com>
2024-03-10 15:47:35 +0530

    New changes

--- modules/EKS/eks-cluster.tf
+++ modules/EKS/eks-cluster.tf
@@ -18,9 +18,9 @@ resource "aws_eks_node_group" "my-node-group" {
   node_role_arn   = aws_iam_role.eks-worker-node-role.arn
 
   scaling_config {
-    desired_size = 3
+    desired_size = 2
     max_size     = 4
-    min_size     = 2
+    min_size     = 1
   }
 
   update_config {
